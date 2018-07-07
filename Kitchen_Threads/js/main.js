1e993768d527423808f5c68ae93de1f10a7b0a95
Sagar Pandav <pandavsagar123@gmail.com>
2018-07-08 00:20:57 +0530

    Created Static component of site

---
+++ Kitchen_Threads/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by PANDAV on 07-07-2018.
+ **/
+
+const IMG = document.querySelectorAll(".single_menu_item_img");
+for (let i =0; i<IMG.length; i++){
+    IMG[i].style.width = "500px";
+    IMG[i].style.height = "200px";
+}
